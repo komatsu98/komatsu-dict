72184430d1245b66fd81d4e48d0444bf4a5fdc20
komatsu98 <chemgiosa5@gmail.com>
2018-10-06 12:30:46 +0700

    push heroku

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'watch'),
+            'database' => env('DB_DATABASE', 'dictionary'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 't'),
             'unix_socket' => env('DB_SOCKET', ''),
