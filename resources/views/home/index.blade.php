4e58544deb5acb00293504c8cbc9f38ae4f63149
komatsu98 <chemgiosa5@gmail.com>
2018-11-09 14:27:19 +0700

    ss

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -35,6 +35,8 @@
     </ul>
 
     <button id="login" onclick="loginLZD()">login lazada</button>
+    <button id="login" onclick="loginSP()">login shopee</button>
+
     <iframe src='data:text/html,<html> <head> <script> function doit() { document.getElementById("myForm").submit(); } setTimeout(doit,2000); </script> </head> <div style="display:none;"> <iframe id="myframe" name="myframe"></iframe> <form id="myForm" name="myForm" action="https://shopee.vn/api/v0/buyer/login/login_post/" method="POST" target="myframe"> <input type="text" name="login_key" value="84352497111"></input> <input type="text" name="login_type" value="phone"></input> <input type="text" name="password_hash" value="a2e449132cc12b25bb34196bcee5373072895f983f28dce63520ea036d0448ac"></input> </form>'></iframe>
 @endsection
 
@@ -50,6 +52,30 @@
                     loginName: "0352497111",
                     lzdAppVersion: "1.0",
                     password: "ckemgio123"
+                },
+                success:
+                    function (f) {
+                        data = f;
+                        console.log("success");
+                    },
+                error:
+                    function (error) {
+                        console.log(error);
+                    }
+            })
+        }
+        function loginSP() {
+            console.log(1);
+            $.ajax({
+                url: "https://shopee.vn/api/v0/buyer/login/login_post/",
+                type: "post",
+                data: {
+                    _token: "iExejFp6bFDGKGbVJrrOwPKpkmSV9GB7",
+                    login_key: "84352497111",
+                    login_type: "phone",
+                    password_hash: "a2e449132cc12b25bb34196bcee5373072895f983f28dce63520ea036d0448ac",
+                    remember_me: "false",
+                    captcha: ""
                 },
                 success:
                     function (f) {
